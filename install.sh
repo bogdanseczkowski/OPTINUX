b785e992923fde3765836a638c64e1fd53cfa4d7
LINUX STRIPPER <bogdan.seczkowski@gmail.com>
2018-05-27 18:53:37 +0200

    File format correct

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ cd gentoo
 
 builddate=$(curl -s http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64/ | sed -nr 's/.*href="stage3-amd64-([0-9].*).tar.xz">.*/\1/p')
 wget http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64/stage3-amd64-$builddate.tar.xz
-tar xpf stage3-*.tar.{bz2,xz} --xattrs-include='*.*' --numeric-owner
+tar xpf stage3-* --xattrs-include='*.*' --numeric-owner
 rm -f stage3*
 
 cp /etc/resolv.conf etc
