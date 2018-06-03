4234899c3295b20614ae90c19c322e58a0c3672a
Bogdan Seczkowski <bogdan.seczkowski@gmail.com>
2018-06-03 18:53:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ./fs.sh
+cd gentoo
 builddate=$(curl -s http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64/ | sed -nr 's/.*href="stage3-amd64-([0-9].*).tar.xz">.*/\1/p')
 wget http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64/stage3-amd64-$builddate.tar.xz
 tar xpf stage3-* --xattrs-include='*.*' --numeric-owner
