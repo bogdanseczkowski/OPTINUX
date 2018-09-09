ca9e40de4494e0695402e5bddd2632f526901019
mlunax <mlunaelumen@gmail.com>
2018-09-09 17:50:24 +0200

    etc-update and nproc support

--- chroot.sh
+++ chroot.sh
@@ -2,17 +2,21 @@
 emerge-webrsync
 eselect profile set "default/linux/amd64/17.0/hardened"
 emerge cpuid2cpuflags
+np=$(nproc)
+enp= `expr $np + 1`
 echo '
 CFLAGS="-O3 -march=native -pipe"
 CXXFLAGS="\${CFLAGS}"
 EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y"
-MAKEOPTS="-j8"' >> /etc/portage/make.conf
+MAKEOPTS="-j$enp"' >> /etc/portage/make.conf
 CPU=$(cpuid2cpuflags)
 out="${CPU//': '/=\"}"
 echo "$out" \" >> /etc/portage/make.conf
 
 emerge  world
 
+emerge gentoo-sources genkernel
+echo -e "y\n" | etc-update --automode -3
 emerge gentoo-sources genkernel
 wget https://raw.githubusercontent.com/bogdanseczkowski/STRIP-LINUX/master/config/4.14/config.amd64
 genkernel --kernel-config=config.amd64 all
